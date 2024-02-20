3fab6b1f3946bd3f9231f1172dfdf802ec296c3e
Zyurkalov <Zyurkalov@gmail.com>
2024-02-20 17:14:44 +0500

    доделал stack

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -11,18 +11,42 @@ import "../../index.css"
 export const StackPage: React.FC = () => {
   type TStackElem = {
     value: string;
-    state: ElementStates.Changing
+    state: ElementStates
+  }
+  const newStack = useMemo(() => new Stack<TStackElem >(), []);
+  const {push, pop, peak, remove, getSize, getContainer} = newStack
+
+  const [input, setInput] = useState('');
+  const [update, setUpdate] = useState(false);
+  const timeout = 500;
+  
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+  };
+  const onPush =  (value: string) => {
+    push({value: value, state: ElementStates.Changing})
+    setInput('')
+
+    setTimeout(() => {
+      if (peak() !== null) {
+        peak()!.state = ElementStates.Default
+      }
+      setUpdate(!update)
+    }, timeout);
+  }
+  const onPop = () => {
+    if (peak() !== null) {
+      peak()!.state = ElementStates.Changing
+    }
+    setUpdate(!update)
+
+    setTimeout(() => {
+      const upd = !update
+      pop()
+      setUpdate(!upd)
+    }, timeout);
   }
 
-  const newStack = useMemo(() => new Stack<string>(), []);
-  const {push, pop, remove, getSize} = newStack
-
-  const [value, setValue] = useState('');
-  const [update, setUpdate] = useState(false);
-
-  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
-  };
   const showTop = (index: number) => {
     if( index === getSize()-1) {
       return 'top'
@@ -33,15 +57,15 @@ export const StackPage: React.FC = () => {
     <SolutionLayout title="Стек">
       <div className="box-container">
         <div className={`input-box ${style["input-box__stackPage"]}`}>
-          <Input placeholder="Введите значение" isLimitText={true} maxLength={4} type={'text'} onChange={onChange} value={value}></Input>
-          <Button text='Добавить' onClick={() => {push(value); setUpdate(!update); setValue('')}} disabled={!value}></Button>
-          <Button text='Удалить' onClick={() => {pop(); setUpdate(!update)}} disabled={getSize() <= 0}></Button>
+          <Input placeholder="Введите значение" isLimitText={true} maxLength={4} type={'text'} onChange={onChange} value={input}></Input>
+          <Button text='Добавить' onClick={() => onPush(input)} disabled={!input}></Button>
+          <Button text='Удалить' onClick={() => onPop()} disabled={getSize() <= 0}></Button>
           <Button text='Очистить' onClick={() => {remove(); setUpdate(!update)}} extraClass={"ml-40"} disabled={getSize() <= 0}></Button>
         </div>
         <ul className="list-box">
-          {newStack.getContainer() !== null
-              ? newStack.getContainer().map((elem, index) => {
-                  return <li className="list-box_circle" key={index}><Circle letter={elem} index={index} head={showTop(index)}/*state={elem.status}*/></Circle></li>;
+          {getContainer() !== null
+              ? getContainer().map((elem, index) => {
+                  return <li className="list-box_circle" key={index}><Circle letter={elem.value} index={index} head={showTop(index)} state={elem.state}></Circle></li>;
                 })
               : null
           }
