488210bbbc3b1e10392990c9faeaf69269b5e4f3
Zyurkalov <Zyurkalov@gmail.com>
2024-03-10 21:02:54 +0500

    первые Unit-тесты

---
+++ src/components/ui/button/button.test.tsx
@@ -0,0 +1,55 @@
+import { render, screen } from '@testing-library/react';
+import renderer from "react-test-renderer";
+import userEvent from '@testing-library/user-event';
+import { Button } from './button';
+
+describe("Button ui-component", () => {
+
+    it('рендерится c текстом', () => {
+        const buttonText = "Click me"
+
+        const tree = renderer
+            .create(<Button text = {buttonText} />)
+            .toJSON();
+            expect(tree).toMatchSnapshot();
+    }); 
+    
+    it('рендерится без текста', () => {
+        const tree = renderer
+            .create(<Button />)
+            .toJSON();
+            expect(tree).toMatchSnapshot();
+    }); 
+
+    it('вызывает колбек функцию при клике', () => {
+        const handleClick = jest.fn();
+        render(<Button data-testid = 'buttonTest' onClick={handleClick}/>)
+
+        const buttonElement = screen.getByTestId('buttonTest');
+        userEvent.click(buttonElement)
+
+        expect(handleClick).toBeCalledTimes(1);
+    }); 
+
+    it('заблокирована и не активна', () => {
+        const handleClick = jest.fn();
+        render(<Button data-testid = 'buttonTest' onClick={handleClick} disabled={true}/>)
+
+        const buttonElement = screen.getByTestId('buttonTest');
+        userEvent.click(buttonElement)
+
+        expect(handleClick).not.toBeCalled();
+        expect(buttonElement).toHaveAttribute('disabled');
+    }); 
+
+    it('заблокирована индикатором загрузки и не активна', () => {
+        const handleClick = jest.fn();
+        render(<Button data-testid = 'buttonTest' onClick={handleClick} isLoader={true}/>)
+
+        const buttonElement = screen.getByTestId('buttonTest');
+        userEvent.click(buttonElement)
+
+        expect(handleClick).not.toBeCalled();
+        expect(buttonElement).toHaveAttribute('disabled');
+    });
+})
